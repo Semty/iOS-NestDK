29a038365b9818b3aebeb8a30d1041e7dadf3526
Vyrastas <jbumgardner@google.com>
2017-06-02 15:55:35 -0700

    Replacing Firebase with REST, added hvac_mode

---
+++ iOS-NestDK/RESTManager.h
@@ -0,0 +1,13 @@
+//
+//  RESTManager.h
+//  iOS-NestDK
+//
+//  Created by Jeffrey Bumgardner on 5/30/17.
+//  Copyright © 2017 Nest Labs. All rights reserved.
+//
+
+#ifndef RESTManager_h
+#define RESTManager_h
+
+
+#endif /* RESTManager_h */
---
+++ iOS-NestDK/RESTManager.m
@@ -0,0 +1,9 @@
+//
+//  RESTManager.m
+//  iOS-NestDK
+//
+//  Created by Jeffrey Bumgardner on 5/30/17.
+//  Copyright © 2017 Nest Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
